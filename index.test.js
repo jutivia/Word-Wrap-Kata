abf2d0fcb1bee8c1a989585784107177d7472b64
Jumoke Boladale-Lawal <76624942+jutivia@users.noreply.github.com>
2022-11-15 08:14:18 +0100

    test: adding an extra test case and changing test description format

--- index.test.js
+++ index.test.js
@@ -1,37 +1,42 @@
 const Wrapper = require("./index.js");
 
-test("wrapingTwoWordsAfterCol", () => {
+test("wraping_two_words_between_col", () => {
   expect(Wrapper.wrap("wrap wrap", 6)).toBe("wrap\nwrap");
 });
 
-test("wrapingThreeWordsBetweenCol", () => {
+test("wraping_three_words_between_col", () => {
   expect(Wrapper.wrap("wrap wrap wrap", 6)).toBe("wrap\nwrap\nwrap");
 });
 
-test("wrappingStringShorterThanCol", () => {
+test("wrapping_a_string_shorter_than_col", () => {
   expect(Wrapper.wrap("wrap", 6)).toBe("wrap");
 });
 
-test("wrappingEmptyString", () => {
+test("wrapping_empty_string", () => {
   expect(Wrapper.wrap("", 1)).toBe("");
 });
 
-test("wrappingOneWordLongerThanCol", () => {
+test("wrapping_on_word_longer_than_col", () => {
   expect(Wrapper.wrap("wrap", 2)).toBe("wr\nap");
 });
 
-test("wrappingOneWordALotLongerThanCol", () => {
+test("wrapping_one_word_a_lot_longer_than_col", () => {
   expect(Wrapper.wrap("professionals", 3)).toBe("pro\nfes\nsio\nnal\ns");
 });
 
-test("wrapingTwoWordsInBetweenCol", () => {
+test("wraping_two_words_in_between_col", () => {
   expect(Wrapper.wrap("wrap worker", 5)).toBe("wrap\nworke\nr");
 });
 
-test("wrapingTwoWordsWithEachshorterThanCol", () => {
+test("wraping_two_words_with_each_shorter_than_col", () => {
   expect(Wrapper.wrap("wrap wrap", 3)).toBe("wra\np\nwra\np");
 });
 
-test("wrappingTwoWordsWithEachSameLengthAsCol", () => {
+test("wrapping_two_words_with_each_same_length_as_col", () => {
   expect(Wrapper.wrap("wrap wrap", 4)).toBe("wrap\nwrap");
 });
+
+test("wrapping_words_with_each_different_lengths", () => {
+  expect(Wrapper.wrap("wrap pores in a warm blanket for a stupendiously long time", 4)).toBe("wrap\npore\ns\nin\na\nwarm\nblan\nket\nfor\na\n\stup\nendi\nousl\ny\nlong\ntime");
+});
+
